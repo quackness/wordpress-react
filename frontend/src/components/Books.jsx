db90c94d72d6bff9527eccd30ef64152b363dc90
Karolina Redden <dubajkaro@gmail.com>
2023-01-04 19:05:51 +0100

    add useState and useEffect for Books

--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,5 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Books () {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/wp-json/wp/v2/books`)
+    .then(function(res) {
+      setBooks([...res.data])
+    .catch(err => console.log(err))
+  })
+}, [])
+console.log(books)
   return (
-    <div> Books list</div>
+   
+    <div> Books list </div>
   )
 }
