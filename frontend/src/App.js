a8c45788005033a7898aab3b8629664ae3969d35
Karolina Redden <dubajkaro@gmail.com>
2023-01-04 18:27:56 +0100

    import Books component: git add ../App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import Books from './components/books';
 
 function App() {
   return (
     <div className="App">
+      <h1>App</h1>
+
+      <Books />
       
     </div>
   );
