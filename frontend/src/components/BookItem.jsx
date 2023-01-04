6453199d2d4c83ad176f0f38112eb72bbb2b6f61
Karolina Redden <dubajkaro@gmail.com>
2023-01-04 20:50:19 +0100

    set up state and import hooks

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+
 export default function BookItem () {
+  const [bookItem, setBookItem] = useState([]);
   return (
     <div>Single book data</div>
 
