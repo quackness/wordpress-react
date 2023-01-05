31faa7f8448e52280f96f19adcf46c0a41cd4893
Karolina Redden <dubajkaro@gmail.com>
2023-01-05 11:56:58 +0100

    format code

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,13 +1,7 @@
-import { useState, useEffect } from "react";
-import axios from 'axios';
-
-
 export default function BookItem (props) {
   const { book } = props;
 
   return (
     <div dangerouslySetInnerHTML={{__html: book.excerpt.rendered}} /> 
-
   )
-
 }
--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,27 +1,25 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import BookItem from './BookItem';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BookItem from "./BookItem";
 
-
-export default function Books () {
-
+export default function Books() {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    axios.get(`/wp-json/wp/v2/books`)
-    .then(function(res) {
-      setBooks([...res.data])
-    })
-    .catch(err => console.log(err))
-}, [])
-console.log(books)
+    axios
+      .get(`/wp-json/wp/v2/books`)
+      .then(function (res) {
+        setBooks([...res.data]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(books);
   return (
-    <div> { books.map( book => (
-      <BookItem key={book.id} book={book}/>
-      // <p key = {book.id}>
-      // {book.title.rendered}
-      // </p>
+    <div>
+      {" "}
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
       ))}
     </div>
-  )
+  );
 }
