2d423c3ba2a3b1212c7c140c8ab91ee4563096f5
Karolina Redden <dubajkaro@gmail.com>
2023-01-05 18:51:26 +0100

    format code

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,38 +1,36 @@
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-
-export default function BookItem (props) {
+export default function BookItem(props) {
   const { book } = props;
   // console.log("test", book.featured_media)
 
   const [author, setAuthor] = useState([]);
   const [image, setImage] = useState("");
 
- useEffect(() => {
-      axios.get(`/wp-json/wp/v2/users/${author}`)
+  useEffect(() => {
+    axios.get(`/wp-json/wp/v2/users/${author}`).then(function (res) {
+      setAuthor([...res.data[0].name]);
+      console.log("author res", res.data);
+    });
+    axios
+      .get(`/wp-json/wp/v2/media/${book.featured_media}`)
       .then(function (res) {
-        setAuthor([...res.data[0].name])
-        console.log("author res", res.data)
-      })
-      axios.get(`/wp-json/wp/v2/media/${book.featured_media}`)
-      .then(function(res) {
-        console.log("media res", res)
+        console.log("media res", res);
         // console.log(image)
-        setImage(res.data.media_details.sizes.large.source_url)
-      })
+        setImage(res.data.media_details.sizes.large.source_url);
+      });
   }, []);
-
-
 
   return (
     <>
-    <small>Review by: <strong>{author}</strong> </small>
-    <Link to={`/book/${book.id}`}> Read review </Link>
-    <img src={image}></img>
-    <div dangerouslySetInnerHTML={{__html: book.excerpt.rendered}}/> 
-
+      <small>
+        Review by: <strong>{author}</strong>{" "}
+      </small>
+      <Link to={`/book/${book.id}`}> Read review </Link>
+      <img src={image}></img>
+      <div dangerouslySetInnerHTML={{ __html: book.excerpt.rendered }} />
     </>
-  )
+  );
 }
