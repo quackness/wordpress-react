bad5369144fd73ca3128c59754ec88d598340dac
Karolina Redden <dubajkaro@gmail.com>
2023-01-05 10:43:04 +0100

    add book item rendering in the component

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -2,10 +2,19 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 
-export default function BookItem () {
+export default function BookItem (props) {
+  const { book } = props;
   const [bookItem, setBookItem] = useState([]);
+
+  // useEffect(()=> {
+  //   axios.get()
+  //     .then( res => )
+  //     .catch(err => console.log(err))
+  // })
+
+
   return (
-    <div>Single book data</div>
+    <div> {book.title.rendered} {book.excerpt.rendered}</div>
 
   )
 }
