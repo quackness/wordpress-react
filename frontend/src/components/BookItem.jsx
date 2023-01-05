d325deebeb7d979280a4141a56394f4b816657c4
Karolina Redden <dubajkaro@gmail.com>
2023-01-05 11:49:39 +0100

    add dangerouslySetInnerHTML to parse HTML

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -4,17 +4,10 @@ import axios from 'axios';
 
 export default function BookItem (props) {
   const { book } = props;
-  const [bookItem, setBookItem] = useState([]);
-
-  // useEffect(()=> {
-  //   axios.get()
-  //     .then( res => )
-  //     .catch(err => console.log(err))
-  // })
-
 
   return (
-    <div> {book.title.rendered} {book.excerpt.rendered}</div>
+    <div dangerouslySetInnerHTML={{__html: book.excerpt.rendered}} /> 
 
   )
+
 }
