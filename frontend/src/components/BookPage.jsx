ae2ddb8d1ffdcddfea8a0d72d658f3878b7915b4
Karolina Redden <dubajkaro@gmail.com>
2023-01-06 12:49:44 +0100

    add book page title to the page and loading to avoid JS crashing when page first loads

--- frontend/src/components/BookPage.jsx
+++ frontend/src/components/BookPage.jsx
@@ -5,20 +5,30 @@ import { useParams } from 'react-router';
 
 
 export default function BookPage (props) {
-const { id } = useParams();
+const { id  } = useParams();
 
-  const [bookPage, setBookPage] = useState({});
-  // console.log(bookPage.id)
+  const [bookPage, setBookPage] = useState({
+    title: {rendered:'Loading'}
+  });
+
+  // console.log(bookPage.title.rendered)
 
   useEffect(() => {
     axios.get(`/wp-json/wp/v2/books/${id}`)
       .then(function(res) {
         console.log("res", res)
         setBookPage(res.data)
+        // setLoaded(true)
       })
+      .catch((err) => console.log(err));
   }, []);
 
+  // if (!loaded) {
+  //   return (<>loading...</>)
+  // }
   return (
-    <div> {bookPage.id} </div>
+    <div> 
+      {bookPage.title.rendered} 
+    </div>
   )
 }
