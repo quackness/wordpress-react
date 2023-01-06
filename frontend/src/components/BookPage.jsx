ee907e86aa1649d2a73aa683c55674f8f4e0c582
Karolina Redden <dubajkaro@gmail.com>
2023-01-06 13:00:52 +0100

    add bookPage content

--- frontend/src/components/BookPage.jsx
+++ frontend/src/components/BookPage.jsx
@@ -1,23 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from 'react-router';
-//for the Router to pass params from the link to the component 
+import { useParams } from "react-router";
+//for the Router to pass params from the link to the component
 
-
-export default function BookPage (props) {
-const { id  } = useParams();
+export default function BookPage(props) {
+  const { id } = useParams();
 
   const [bookPage, setBookPage] = useState({
-    title: {rendered:'Loading'}
+    title: { rendered: "Loading" },
+    acf: { publisher: "Loading" },
+    content: { rendered: "Loading" },
   });
 
   // console.log(bookPage.title.rendered)
 
   useEffect(() => {
-    axios.get(`/wp-json/wp/v2/books/${id}`)
-      .then(function(res) {
-        console.log("res", res)
-        setBookPage(res.data)
+    axios
+      .get(`/wp-json/wp/v2/books/${id}`)
+      .then(function (res) {
+        console.log("res", res);
+        setBookPage(res.data);
         // setLoaded(true)
       })
       .catch((err) => console.log(err));
@@ -27,8 +29,10 @@ const { id  } = useParams();
   //   return (<>loading...</>)
   // }
   return (
-    <div> 
-      {bookPage.title.rendered} 
-    </div>
-  )
+    <>
+      <h1>{bookPage.title.rendered}</h1>
+      <div dangerouslySetInnerHTML={{ __html: bookPage.content.rendered }} />
+      <h4>Publisher: {bookPage.acf.publisher}</h4>
+    </>
+  );
 }
