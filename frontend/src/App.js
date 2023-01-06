ccfd750b6bad9359e253188a0ec2f2f013db3db1
Karolina Redden <dubajkaro@gmail.com>
2023-01-06 14:32:22 +0100

    apply basic css to the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Books from "./components/Books";
+import BookPage from "./components/BookPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route exact path='/' element={<Books />} />
-      {/* <Route exact path="book/:id" component= */}
-      </Routes> 
+        <Route exact path='/' element={<Books />} />
+        <Route exact path='/book/:id' element={<BookPage />} />
+      </Routes>
     </Router>
   );
 }
