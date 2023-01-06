76b366bc7dcfb8e1c127ceb01af81967790c79f0
Karolina Redden <dubajkaro@gmail.com>
2023-01-06 12:24:05 +0100

    add useParams to catch the book id from the Router url, add axios call to fetch the res with a book

--- frontend/src/components/BookPage.jsx
+++ frontend/src/components/BookPage.jsx
@@ -1,5 +1,24 @@
-export default function BookPage () {
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router';
+//for the Router to pass params from the link to the component 
+
+
+export default function BookPage (props) {
+const { id } = useParams();
+
+  const [bookPage, setBookPage] = useState({});
+  // console.log(bookPage.id)
+
+  useEffect(() => {
+    axios.get(`/wp-json/wp/v2/books/${id}`)
+      .then(function(res) {
+        console.log("res", res)
+        setBookPage(res.data)
+      })
+  }, []);
+
   return (
-    <div> TEST </div>
+    <div> {bookPage.id} </div>
   )
 }
