9326d246bfe4aa2265786e0311dea1414c70a5d7
Karolina Redden <dubajkaro@gmail.com>
2023-01-11 16:38:03 +0100

    add state for the image

--- frontend/src/components/AddBook.jsx
+++ frontend/src/components/AddBook.jsx
@@ -1,38 +1,67 @@
-export default function AddBook (props) {
-  const {books} = props;
+import {useState} from 'react';
 
+export default function AddBook(props) {
+  const { books } = props;
 
-
- 
+  const [imageURL, setImageURL ] = useState([])
+  
 
   return (
     <>
-      
-<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addBook">
-  Add a book
-</button>
+      <button
+        type="button"
+        className="btn btn-success"
+        data-bs-toggle="modal"
+        data-bs-target="#addBook"
+      >
+        Add a book
+      </button>
 
-
-<div class="modal fade" id="addBook" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div
+        class="modal fade"
+        id="addBook"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Add a new book
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="mb-3">
+                  <label for="image" class="form-label">
+                    Image
+                  </label>
+                  <input type="url" class="form-control" id="image" value={imageURL} onChange={e => e.target.value(setImageURL)}/>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
     </>
-  )
+  );
 }
