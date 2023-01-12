774496c21780e3bd71490b480653eab2d216fdba
Karolina Redden <dubajkaro@gmail.com>
2023-01-12 16:14:02 +0100

    add edit book form and state

---
+++ frontend/src/components/EditBook.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+
+
+export default function EditBook(props) {
+  const { book, setBooks, books } = props;
+  console.log("edit", book)
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [excerpt, setExcerpt] = useState("");
+
+  function onSubmitForm() {
+
+  }
+
+  return (
+    <>
+      <div>
+        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editBook">
+          Edit a book
+        </button>
+      </div>
+
+      <div
+        class="modal fade"
+        id="editBook"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Edit book
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <form onSubmit={onSubmitForm}>
+              <div class="modal-body">
+                <div class="mb-3">
+                  <label for="title" class="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+
+                  <label for="content" class="form-label">
+                    Content
+                  </label>
+
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="content"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+
+                  <label for="excerpt" class="form-label">
+                    Excerpt
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="excerpt"
+                    value={excerpt}
+                    onChange={(e) => setExcerpt(e.target.value)}
+                  />
+
+                  {/* <label for="image" class="form-label">
+                    Image
+                  </label>
+                  <input type="file" class="form-control" id="file" /> */}
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                
+                >
+                  Edit a book
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
